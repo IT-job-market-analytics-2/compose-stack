8af974f2f6f8f64352d2f83ca4c76eb891cf2516
Ilya <freer1der.msf@gmail.com>
2024-04-09 14:27:19 +0300

    Added scripts to create exchange, queues and bindings in rabbitmq_create_objects.sh. Added .gitignore.
    Added script to create exchange for scheduled tasks.
    Added scripts to create queues and bindings for vacancy import service and analytics builder service.
    Added .gitignore file to ignore IntellijIdea's files.

--- rabbitmq_create_objects.sh
+++ rabbitmq_create_objects.sh
@@ -9,6 +9,39 @@ function create_queue {
     }'
 }
 
+function create_exchange {
+    curl --request PUT \
+    --url "http://localhost:15672/api/exchanges/%2F/$1" \
+    --header 'Authorization: Basic YW5hbHl0aWNzX3NlcnZpY2U6cGFzc3dvcmQ=' \
+    --data-raw "{
+        \"type\": \"$2\",
+        \"auto_delete\": false,
+        \"durable\": true,
+        \"arguments\": {}
+    }"
+}
+
+function create_binding {
+    curl --request POST \
+    --url "http://localhost:15672/api/bindings/%2F/e/$1/q/$2" \
+    --header 'Authorization: Basic YW5hbHl0aWNzX3NlcnZpY2U6cGFzc3dvcmQ=' \
+    --data-raw "{
+        \"routing_key\": \"$3\",
+        \"arguments\": {}
+    }"
+}
+
+# Scheduler service exchange
+create_exchange scheduled-tasks-exchange direct
+
+# Vacancy import service task consumer
+create_queue vacancy-import-scheduled-tasks-queue
+create_binding scheduled-tasks-exchange vacancy-import-scheduled-tasks-queue vacancy-import-task
+
+# Analytics builder service task consumer
+create_queue analytics-builder-scheduled-tasks-queue
+create_binding scheduled-tasks-exchange analytics-builder-scheduled-tasks-queue analytics-builder-task
+
 create_queue scheduled-tasks-queue
 create_queue imported-vacancies-queue
 create_queue new-vacancies-queue
